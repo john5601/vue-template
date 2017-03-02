df214aace6cc6f61520b541a5b6a0099aebde9df
Osaki Hsieh - mac <aspdoctor@gmail.com>
2017-03-02 11:23:02 +0800

    add gulpfile.js

---
+++ template/gulpfile.js
@@ -0,0 +1,107 @@
+const gulp = require('gulp'),
+  prettyError = require('gulp-prettyerror'),
+  htmlBeautify = require('gulp-html-beautify'),
+  imagemin = require('gulp-imagemin'),
+  sass = require('gulp-sass'),
+  autoprefixer = require('gulp-autoprefixer'),
+  sourcemaps = require('gulp-sourcemaps'),
+  bourbon = require("node-bourbon").includePaths,
+  cleanCSS = require('gulp-clean-css'),
+  cssImageDimensions = require("gulp-css-image-dimensions"),
+  path = require('path'),
+  zip = require('gulp-zip')
+
+//------ path
+let paths = {
+  scss: {
+    watch: './scss/**/*.{scss, sass}',
+    src: './scss/**/!(_)*.{scss,sass}',
+    dest: './static/style/'
+  },
+  ico: {
+    src: './dist/static/favicon.ico',
+    dest: './dist/'
+  },
+  images: {
+    src: './dist/static/images/',
+    dest: './dist/static/images/'
+  },
+  html: {
+    src: './dist/*.html',
+    dest: './dist/'
+  }
+};
+
+//------ directive
+
+let buildIcon = () => {
+  return gulp
+    .src(paths.ico.src)
+    .pipe(prettyError())
+    .pipe(gulp.dest(paths.ico.dest))
+}
+
+let buildImage = () => {
+  return gulp
+    .src(paths.images.src)
+    .pipe(prettyError())
+    .pipe(imagemin())
+    .pipe(gulp.dest(paths.images.dest))
+}
+
+let buildHtml = () => {
+  return gulp
+    .src(paths.html.src)
+    .pipe(prettyError())
+    .pipe(htmlBeautify({
+      indent_size: 2
+    }))
+    .pipe(gulp.dest(paths.html.dest))
+}
+
+gulp.task('build:html', buildHtml)
+
+let devStyles = () => {
+  return gulp
+    .src(paths.scss.src)
+    // .pipe(prettyError())
+    .pipe(sourcemaps.init())
+    .pipe(sass.sync({
+      includePaths: bourbon
+    }).on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 5 versions', '> 5%']
+    }))
+    // .pipe(cssImageDimensions())
+    .pipe(cleanCSS())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(paths.scss.dest))
+}
+
+gulp.task('build:styles', devStyles)
+
+gulp.task('watch:styles', () => {
+  gulp.watch(paths.scss.watch, devStyles).on('change', (path) => {
+    console.log(`${path} on Changed!`);
+  });
+});
+
+let buildZip = () => {
+  let
+    today = new Date(),
+    month = today.getMonth()+1,
+    day = today.getDate()
+  return gulp
+    .src('./dist/**/*')
+    .pipe( zip(`build-${month}-${day}.zip`) )
+    .pipe(gulp.dest('./dist'));
+}
+
+gulp.task('build:zip', buildZip)
+
+gulp.task('default', gulp.series(
+  buildHtml,
+  buildImage,
+  buildIcon
+  // buildZip
+))
