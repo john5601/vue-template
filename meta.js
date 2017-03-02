c26c626b02d50ab19655c44f137fb97b28ef93da
Osaki Hsieh - mac <aspdoctor@gmail.com>
2017-03-02 14:12:24 +0800

    globalStyle

--- meta.js
+++ meta.js
@@ -59,14 +59,15 @@ module.exports = {
       "type": "confirm",
       "message": "要安裝 Gulp#4.0 嗎?"
     },
-    "cssReset": {
+    "globalStyle": {
       "when": "needGulp",
       "type": "confirm",
-      "message": "使用 CSS RESET? \r\n(需使用 Gulp)"
-    },
-    "globalStyle": {
-      "type": "confirm",
       "message": "使用全域 CSS?"
+    },
+    "cssReset": {
+      "when": "needGulp && globalStyle",
+      "type": "confirm",
+      "message": "使用 CSS RESET? \r\n(需使用 Gulp)"
     },
     "relativePath": {
       "type": "confirm",
